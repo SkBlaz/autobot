22b0f09a1eafc711430939d53db4e582ec824705
skblaz <skrljblaz@gmail.com>
2021-04-15 20:41:28 +0200

    Spanish example

---
+++ examples/example_spanish.py
@@ -0,0 +1,14 @@
+## A simple example showcasing the minimal usecase of autoBOTLib on an insults classification data.
+
+import autoBOTLib
+import pandas as pd
+
+## Load example data frame
+dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
+train_sequences = dataframe['text_a'].values.tolist()
+train_targets = dataframe['label'].values
+
+autoBOTLibObj = autoBOTLib.GAlearner(train_sequences,
+                               train_targets,
+                               time_constraint=0.1).evolve()
+
