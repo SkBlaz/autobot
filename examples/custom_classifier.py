f2d4d0efd3b69089c08007e8bb1010d653a0b664
CitizenIsErased <timi.sp@gmail.com>
2021-05-03 16:59:47 +0200

    windows multiprocessing

--- examples/custom_classifier.py
+++ examples/custom_classifier.py
@@ -2,39 +2,43 @@ import autoBOTLib
 import pandas as pd
 from sklearn.linear_model import SGDClassifier
 
-## Load example data frame
-dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
-train_sequences = dataframe['text_a'].values.tolist()
-train_targets = dataframe['label'].values
+def run():
+    ## Load example data frame
+    dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
+    train_sequences = dataframe['text_a'].values.tolist()
+    train_targets = dataframe['label'].values
 
-## The syntax for specifying a learner and the hyperparameter space!
-## These are the hyperparameters to be explored for each representation.
-classifier_hyperparameters = {
-    "loss": ["hinge"],
-    "penalty": ["elasticnet"],
-    "alpha": [0.01, 0.001],
-    "l1_ratio": [0, 0.001, 1]
-}
+    ## The syntax for specifying a learner and the hyperparameter space!
+    ## These are the hyperparameters to be explored for each representation.
+    classifier_hyperparameters = {
+        "loss": ["hinge"],
+        "penalty": ["elasticnet"],
+        "alpha": [0.01, 0.001],
+        "l1_ratio": [0, 0.001, 1]
+    }
 
-## This is the classifier compatible with the hyperparameters.
-custom_classifier = SGDClassifier()
+    ## This is the classifier compatible with the hyperparameters.
+    custom_classifier = SGDClassifier()
 
-autoBOTLibObj = autoBOTLib.GAlearner(
-    train_sequences,  # input sequences
-    train_targets,  # target space 
-    time_constraint=1,  # time in hours
-    num_cpu=4,  # number of CPUs to use
-    task_name="example test",  # task identifier
-    hof_size=3,  # size of the hall of fame
-    top_k_importances=25,  # how many top features to output as final ranking
-    memory_storage="./memory",
-    representation_type="symbolic",
-    classifier=custom_classifier,
-    classifier_hyperparameters=classifier_hyperparameters
-)  # or neurosymbolic or neural
+    autoBOTLibObj = autoBOTLib.GAlearner(
+        train_sequences,  # input sequences
+        train_targets,  # target space 
+        time_constraint=1,  # time in hours
+        num_cpu=4,  # number of CPUs to use
+        task_name="example test",  # task identifier
+        hof_size=3,  # size of the hall of fame
+        top_k_importances=25,  # how many top features to output as final ranking
+        memory_storage="./memory",
+        representation_type="symbolic",
+        classifier=custom_classifier,
+        classifier_hyperparameters=classifier_hyperparameters
+    )  # or neurosymbolic or neural
 
-autoBOTLibObj.evolve(
-    nind=10,  ## population size
-    strategy="evolution",  ## optimization strategy
-    crossover_proba=0.6,  ## crossover rate
-    mutpb=0.4)  ## mutation rate
+    autoBOTLibObj.evolve(
+        nind=10,  ## population size
+        strategy="evolution",  ## optimization strategy
+        crossover_proba=0.6,  ## crossover rate
+        mutpb=0.4)  ## mutation rate
+        
+if __name__ == "__main__":
+    run()
--- examples/minimal.py
+++ examples/minimal.py
@@ -3,16 +3,20 @@
 import autoBOTLib
 import pandas as pd
 
-## Load example data frame
-dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
-train_sequences = dataframe['text_a'].values.tolist()
-train_targets = dataframe['label'].values
+def run():
+    ## Load example data frame
+    dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
+    train_sequences = dataframe['text_a'].values.tolist()
+    train_targets = dataframe['label'].values
 
-autoBOTLibObj = autoBOTLib.GAlearner(train_sequences,
-                                     train_targets,
-                                     n_fold_cv = 3,
-                                     time_constraint=0.1).evolve()
+    autoBOTLibObj = autoBOTLib.GAlearner(train_sequences,
+                                         train_targets,
+                                         n_fold_cv = 3,
+                                         time_constraint=0.1).evolve()
 
-dataframe2 = pd.read_csv("../data/insults/test.tsv", sep="\t")
-test_sequences = dataframe2['text_a'].values.tolist()
-predictions = autoBOTLibObj.predict(test_sequences)
+    dataframe2 = pd.read_csv("../data/insults/test.tsv", sep="\t")
+    test_sequences = dataframe2['text_a'].values.tolist()
+    predictions = autoBOTLibObj.predict(test_sequences)
+
+if __name__ == "__main__":
+    run()
