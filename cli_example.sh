65a2dcabbd5e22b6fff24e076414f3dcc9d7c984
skblaz <skrljblaz@gmail.com>
2021-09-26 19:59:32 +0200

    Added CLI workflow with dependencies

--- cli_example.sh
+++ cli_example.sh
@@ -1,5 +1,6 @@
 ## Train an autoBOT classifier
-python autoBOTLib --mode learning --datafolder ./data/insults --output_folder results --classifier autoBOT-base --time 1 --hof_size 3 --representation_type neurosymbolic --mutation_rate 0.3 --crossover_rate 0.6
+mkdir results;
+python autoBOTLib --mode learning --datafolder ./data/insults/train.tsv --output_folder results --classifier autoBOT-base --time 0.1 --hof_size 3 --representation_type neurosymbolic-lite --mutation_rate 0.3 --crossover_rate 0.6
 
 ## Obtain predictions from a trained model
 python autoBOTLib --mode prediction --load_model ./results/autoBOTmodel.pickle --predict_data ./data/insults/test.tsv
