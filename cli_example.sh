f196658563e1c9b0749e97e8c470656bddac7e8f
skblaz <skrljblaz@gmail.com>
2021-09-27 05:50:33 +0200

    CLI test, re-added

--- cli_example.sh
+++ cli_example.sh
@@ -3,4 +3,4 @@ mkdir results;
 python autoBOTLib --mode learning --datafolder ./data/insults/train.tsv --output_folder results --classifier autoBOT-base --time 0.1 --hof_size 3 --representation_type neurosymbolic-lite --mutation_rate 0.3 --crossover_rate 0.6
 
 ## Obtain predictions from a trained model
-python autoBOTLib --mode prediction --load_model ./results/autoBOTmodel.pickle --predict_data ./data/insults/test.tsv
+#python autoBOTLib --mode prediction --load_model ./results/autoBOTmodel.pickle --predict_data ./data/insults/test.tsv
