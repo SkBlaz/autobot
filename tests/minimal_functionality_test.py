3bd58ef1048337a7662c562b59a498336541b337
skblaz <skrljblaz@gmail.com>
2021-09-29 15:16:31 +0200

    Tests update

--- tests/minimal_functionality_test.py
+++ tests/minimal_functionality_test.py
@@ -21,7 +21,7 @@ def test_minimal_mlc():
         representation_type=
         "symbolic",  ## See the documentation for all possible representation types.
         n_fold_cv=3,
-        memory_storage="memory",
+        memory_storage="memory2",
         sparsity=0.1,
         upsample=
         False,  ## Suitable for imbalanced data - randomized upsampling tends to help.
@@ -39,8 +39,7 @@ def test_minimal_mlc():
     autoBOTLibObj.generate_report(output_folder="./report/",
                                   job_id="as9y0gb98ss")
 
-
-
+    
 def test_minimal():
     ## Load example data frame
     dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
@@ -57,7 +56,7 @@ def test_minimal():
         sparsity=0.1,
         upsample=
         False,  ## Suitable for imbalanced data - randomized upsampling tends to help.
-        time_constraint=0.2).evolve(
+        time_constraint=0.1).evolve(
             strategy="evolution"
         )  ## strategy = "direct-learning" trains a single learner.
 
@@ -72,25 +71,25 @@ def test_minimal():
                                   job_id="as9y0gb98s")
 
 
-@pytest.mark.parametrize("representation_type",
-                         ["symbolic", "neurosymbolic-default"])
-@pytest.mark.parametrize("fold_number", [2, 3, 4, 5, 6, 7, 8, 9, 10])
-@pytest.mark.parametrize("sparsity", [0.01, 0.05, 0.1, 0.3, 0.6, 0.9, 1])
-@pytest.mark.parametrize("time_constraint", [0.1, 1, 10])
-def test_initializations(fold_number, representation_type, sparsity,
-                         time_constraint):
+# @pytest.mark.parametrize("representation_type",
+#                          ["symbolic", "neurosymbolic-default"])
+# @pytest.mark.parametrize("fold_number", [2, 3, 4, 5, 6, 7, 8, 9, 10])
+# @pytest.mark.parametrize("sparsity", [0.01, 0.05, 0.1, 0.3, 0.6, 0.9, 1])
+# @pytest.mark.parametrize("time_constraint", [0.1, 1, 10])
+# def test_initializations(fold_number, representation_type, sparsity,
+#                          time_constraint):
 
-    dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
-    train_sequences = dataframe['text_a']
-    train_targets = dataframe['label']
-    autoBOTLibObj = autoBOTLib.GAlearner(
-        train_sequences,
-        train_targets,
-        representation_type=
-        representation_type,  ## See the documentation for all possible representation types.
-        n_fold_cv=fold_number,
-        memory_storage="memory2",
-        sparsity=sparsity,
-        upsample=
-        False,  ## Suitable for imbalanced data - randomized upsampling tends to help.
-        time_constraint=time_constraint)
+#     dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
+#     train_sequences = dataframe['text_a']
+#     train_targets = dataframe['label']
+#     autoBOTLibObj = autoBOTLib.GAlearner(
+#         train_sequences,
+#         train_targets,
+#         representation_type=
+#         representation_type,  ## See the documentation for all possible representation types.
+#         n_fold_cv=fold_number,
+#         memory_storage="memory2",
+#         sparsity=sparsity,
+#         upsample=
+#         False,  ## Suitable for imbalanced data - randomized upsampling tends to help.
+#         time_constraint=time_constraint)
