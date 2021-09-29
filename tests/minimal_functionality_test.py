c6dc8b407cd0fa7a780b25bc2f2008d4d9495f3d
skblaz <skrljblaz@gmail.com>
2021-09-29 15:48:56 +0200

    Added initialization tests

--- tests/minimal_functionality_test.py
+++ tests/minimal_functionality_test.py
@@ -71,25 +71,25 @@ def test_minimal():
                                   job_id="as9y0gb98s")
 
 
-# @pytest.mark.parametrize("representation_type",
-#                          ["symbolic", "neurosymbolic-default"])
-# @pytest.mark.parametrize("fold_number", [2, 3, 4, 5, 6, 7, 8, 9, 10])
-# @pytest.mark.parametrize("sparsity", [0.01, 0.05, 0.1, 0.3, 0.6, 0.9, 1])
-# @pytest.mark.parametrize("time_constraint", [0.1, 1, 10])
-# def test_initializations(fold_number, representation_type, sparsity,
-#                          time_constraint):
+@pytest.mark.parametrize("representation_type",
+                         ["symbolic", "neurosymbolic-default"])
+@pytest.mark.parametrize("fold_number", [2, 3, 4, 5, 6, 7, 8, 9, 10])
+@pytest.mark.parametrize("sparsity", [0.01, 0.05, 0.1, 0.3, 0.6, 0.9, 1])
+@pytest.mark.parametrize("time_constraint", [0.1, 1, 10])
+def test_initializations(fold_number, representation_type, sparsity,
+                         time_constraint):
 
-#     dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
-#     train_sequences = dataframe['text_a']
-#     train_targets = dataframe['label']
-#     autoBOTLibObj = autoBOTLib.GAlearner(
-#         train_sequences,
-#         train_targets,
-#         representation_type=
-#         representation_type,  ## See the documentation for all possible representation types.
-#         n_fold_cv=fold_number,
-#         memory_storage="memory2",
-#         sparsity=sparsity,
-#         upsample=
-#         False,  ## Suitable for imbalanced data - randomized upsampling tends to help.
-#         time_constraint=time_constraint)
+    dataframe = pd.read_csv("../data/insults/train.tsv", sep="\t")
+    train_sequences = dataframe['text_a']
+    train_targets = dataframe['label']
+    autoBOTLibObj = autoBOTLib.GAlearner(
+        train_sequences,
+        train_targets,
+        representation_type=
+        representation_type,  ## See the documentation for all possible representation types.
+        n_fold_cv=fold_number,
+        memory_storage="memory2",
+        sparsity=sparsity,
+        upsample=
+        False,  ## Suitable for imbalanced data - randomized upsampling tends to help.
+        time_constraint=time_constraint)
